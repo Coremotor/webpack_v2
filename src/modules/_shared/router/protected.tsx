3f2d816e1afacb71ac6c057b82ba6deca6a8c6de
Андрей Демаков <andrei.demakov@travelline.ru>
2023-09-12 16:35:57 +0300

    fix protected route

--- src/modules/_shared/router/protected.tsx
+++ src/modules/_shared/router/protected.tsx
@@ -1,26 +1,14 @@
-import { useAppSelector } from "modules/_shared/root-store/hooks";
-import { getIsAuth } from "modules/authorization/store/selectors";
+import { keycloakClient } from "modules/_shared/services/keycloak";
 import { FC, ReactNode } from "react";
-import { Navigate, useLocation } from "react-router-dom";
-
-import { RoutesEnum } from "./routes";
 
 type TProtectedProps = {
   children: ReactNode;
 };
 
 export const Protected: FC<TProtectedProps> = ({ children }) => {
-  const auth = useAppSelector(getIsAuth);
-  const location = useLocation();
-
-  if (!auth) {
-    return (
-      <Navigate
-        to={RoutesEnum.Authorization}
-        state={{ from: location }}
-        replace
-      />
-    );
+  if (!keycloakClient.isLogged()) {
+    window.location.reload();
+    return;
   }
 
   return children;
